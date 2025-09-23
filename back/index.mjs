d24e131327856708608dfaa1f79134c515d08e8a
facuschoenfeld <facuschoenfeld@gmail.com>
2025-09-22 21:59:06 -0300

    back changes

---
+++ back/index.mjs
@@ -0,0 +1,25 @@
+// GUIA https://expressjs.com/en/guide/routing.html
+
+// Importar Express
+
+
+// Crear servidor Express
+
+
+// Agregar a express el soporte para JSON
+
+
+// Crear Ruta GET para obtener productos
+
+
+// Crear Ruta POST para crear producto
+
+
+// Crear Ruta PUT para modificar producto
+
+
+// Crear Ruta DELETE para eliminar un producto
+
+
+// Iniciar servidor express
+// Dentro de la función hay que agregar sequelize.sync()
